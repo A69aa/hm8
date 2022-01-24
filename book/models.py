7ab190bd2e87baf17b21ca73477ed8bd3d40e5f1
Aizhamal <aizhtynarova03@gmail.com>
2022-01-24 23:08:42 +0300

    hw5

--- book/models.py
+++ book/models.py
@@ -16,3 +16,45 @@ class Book(models.Model):
     genre = models.CharField(choices=GENRE_CHOICE, max_length=100, null=True)
     data = models.DateField(auto_now_add=True, null=True)
 
+    def __str__(self):
+        return self.title
+
+
+class BookComment(models.Model):
+    books = models.ForeignKey(Book, on_delete=models.CASCADE,
+                              related_name='book_comment')
+    text = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.books.title
+
+
+
+class Expert(models.Model):
+    books = models.ForeignKey(Book, on_delete=models.CASCADE,
+                              related_name='expert')
+    full_name = models.CharField(max_length=50)
+    ACTIVITY = (
+        ('BUSINESSMAN','BUSINESSMAN'),
+        ('BLOGGER','BLOGGER'),
+        ('HOLLYWOOD STAR','HOLLYWOOD STAR'),
+        ('DRIVER','DRIVER'),
+        ('KILLER','KILLER'),
+    )
+    activity = models.CharField(choices=ACTIVITY,max_length=100)
+    info = models.TextField()
+
+    def __str__(self):
+        return str(self.books)
+
+
+
+class ExpertRecomendation(models.Model):
+    books = models.ForeignKey(Book, on_delete=models.CASCADE,
+                              related_name='expert_recommendation')
+    recommendation_text = models.TextField()
+
+    def __str__(self):
+        return str(self.books)
--- book/urls.py
+++ book/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'books'
 urlpatterns = [
-    path('books/',views.book_all, name='book_all'),
-    path('books/<int:id>/', views.book_deteil, name='book_deteil'),
-    path('books/<int:id>/update/', views.book_update, name='book_update'),
-    path('books/<int:id>/delete/', views.book_delete, name='book_delete'),
-    path('add-book/',views.add_book, name='add-book'),
+    path('books/',views.BooksListView.as_view(), name='book_all'),
+    path('books/<int:id>/', views.BooksDeteilView.as_view(), name='book_deteil'),
+    path('books/<int:id>/update/', views.BooksUpdateView.as_view(), name='book_update'),
+    path('books/<int:id>/delete/', views.BooksDeleteView.as_view(), name='book_delete'),
+    path('add-book/',views.BooksCreateView.as_view(), name='add-book'),
 ]
